bb7b2ebb7c3c619ed3a54d9d79525f566749d534
Tau <taumasaire@gmail.com>
2023-10-31 12:01:48 +0200

    magic calculation

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the list
+ * Return: O if theres no cycle, 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+	listint_t *fast = list->next;
+
+	while (head != NULL && fast != NULL && fast->next != NULL)
+	{
+		if (head == fast)
+			return (1);
+
+		head = head->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming"):str.find("Python",str.find("object-oriented programming")) + len("Python")]
+str = str[39:67] + "with " + str[:6]
 print(str)
