342542b276d5ab147ec8f7cca5824c9e77768355
Tau <taumasaire@gmail.com>
2023-11-03 00:55:02 +0200

    revised

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-import py_compile
-
 if __name__ == "__main__":
-    m_code = py_compile.compile("hidden_4.py")
+    import hidden_4
 
-    names = [name for name in m_code.co_names if not name.startswith("__")]
-
-    s_names = sorted(names)
-
-    for name in s_names:
-        print("{}".format(name))
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
