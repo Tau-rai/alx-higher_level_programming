26d736143a00df7e7ffa11f22a53629f22016598
Tau <taumasaire@gmail.com>
2023-11-02 18:38:21 +0200

    print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    nargs = len(sys.argv)
+    cmd = nargs - 1
+
+    if nargs == 1:
+        print("{:d} argument.".format(cmd))
+    elif nargs == 2:
+        print("{:d} argument:\n{:d}: {:s}".format(cmd, cmd, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(cmd))
+        for k in range(1, nargs):
+            print("{:d}: {:s}".format(k, sys.argv[k]))
