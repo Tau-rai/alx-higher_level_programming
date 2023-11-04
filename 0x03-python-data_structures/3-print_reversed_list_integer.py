85b7c89bf98acc429178e4f70ab2d872c8e15b22
Tau <taumasaire@gmail.com>
2023-11-04 17:28:15 +0200

    str.replace

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     reversed_list = my_list[::-1]
-    print("{}".format(reversed_list))
+    for i in range(len(reversed_list)):
+        print("{}".format(reversed_list[i]))
