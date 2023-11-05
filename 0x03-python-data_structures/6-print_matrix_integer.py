3501f4d7ab73b7919a120fd16b5563bb68bd6411
Tau <taumasaire@gmail.com>
2023-11-05 17:43:32 +0200

    Only by two

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-    
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=' ')
-        print()
+        print(" ".join("{:d}".format(num) for num in row))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        return tuple_a
-    elif len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    result = tuple(map(lambda x, y: x + y, tuple_a[:2], tuple_b[:2]))
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
