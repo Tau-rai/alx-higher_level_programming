ed7767427bd0d1024811afcbfdeb7e4b76e0fbc4
Tau <taumasaire@gmail.com>
2023-11-21 00:49:43 +0200

    raise exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    res = None
     try:
         res = a / b
         print("Inside result: {}". format(res))
     except ZeroDivisionError:
+        res = None
         print("Inside result: {}". format(res))
-        return None
     finally:
         return res
