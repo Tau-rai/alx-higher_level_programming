ed1224f7034eabe5e5ee5dc3b0ca975bc868ff0e
Tau <taumasaire@gmail.com>
2023-11-21 23:14:19 +0200

    docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Note: This a an empty class Square that defines a square\
+but does not have any function in it.
+"""
 class Square:
     """This is an empty class"""
     pass
