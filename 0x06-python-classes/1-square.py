c759d54b2a2ed90cd9458f707f5a7fddeb8c8f27
Tau <taumasaire@gmail.com>
2023-11-21 23:40:53 +0200

    sqaure with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,13 @@
 """
 Note: This a an empty class Square that defines a square\
 but does not have any function in it.
+
+Args:
+    size: is a private attribute
 """
 
 
 class Square:
     """This is an empty class"""
-    pass
+    def __init__(self, __size):
+        self.__size = __size
