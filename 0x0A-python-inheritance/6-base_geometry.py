10669c69f06c846ec269691ea287087f160f86a9
Tau <taumasaire@gmail.com>
2023-12-04 17:26:13 +0200

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a Geometry class
+"""
+
+
+class BaseGeometry:
+    """A geometry class
+    """
+    def area(self):
+        """A function that calculates area
+        """
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains a Geometry class
+"""
+
+
+class BaseGeometry:
+    """A geometry class
+    """
+    def area(self):
+        """A function that calculates area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A function that validates the value
+
+        Args:
+            name (string): variable name of the value
+            value (int): an integer
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        else:
+            if value <= 0:
+                raise ValueError("{:s} must be greater than 0".format(name))
