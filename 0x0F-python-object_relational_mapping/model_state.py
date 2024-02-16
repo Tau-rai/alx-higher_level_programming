aa402b856931bd72fc4ebb06bc11109ae31617b7
Tau <taumasaire@gmail.com>
2024-02-16 12:45:27 +0200

    first state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# connect to the database
+DB_URI = 'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
+engine = create_engine(DB_URI, pool_pre_ping=True)
+
+# create a declarative base for mapping classes to db tables
+Base = declarative_base()
+
+
+class State(Base):
+    """A class that inerits from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
+# create the tables in the database
+Base.metadata.create_all(engine)
