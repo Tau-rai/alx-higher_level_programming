6ec0004a531be42f2718375c3117732e09f212f7
Tau <taumasaire@gmail.com>
2024-02-16 18:14:59 +0200

    delete states script

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script deletes all state objects with name containing a
+from the db hbtn_0e_6_usa
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    DB_URI = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        args[1], args[2], args[3]
+    )
+    engine = create_engine(DB_URI, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session(engine)
+
+    # filter the states from the database and delete 
+    states_with_a = session.query(State).filter(State.name.like('%a%')).delete()
+
+    session.commit()
+    session.close()
