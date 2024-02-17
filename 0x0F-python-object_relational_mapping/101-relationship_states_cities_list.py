4b0b37f9a8e9f9bc1355d2fd0664109d168a0428
Tau <taumasaire@gmail.com>
2024-02-17 19:28:29 +0200

    list relationship script

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects and corresponding City objects,
+in the database hbtn_0e_101_usa
+"""
+
+
+import sys
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    DB_URI = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        args[1], args[2], args[3]
+    )
+    engine = create_engine(DB_URI, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session(engine)
+
+    # query the state/city objects and displaying the results
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -6,8 +6,8 @@ database hbtn_0e_101_usa
 
 
 import sys
+from relationship_state import Base
 from model_city import City
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     session = Session(engine)
 
     # querying the state/city objects and displaying the results
-    for city in session.query(City).join(State).order_by(City.id):
+    for city in session.query(City).join(City.state).order_by(City.id):
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
     session.close()
