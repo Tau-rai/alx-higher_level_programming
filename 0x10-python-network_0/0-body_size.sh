89f149496c8d0056c5b178553b15e3c084fb8faf
Tau <taumasaire@gmail.com>
2024-02-29 14:44:26 +0200

    body size script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Takes in a URL and displays the size of the body of the response
 
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request and displays the body
 
-RESPONSE=$(curl -sI "$1")
+# RESPONSE=$(curl -sI "$1")
 
-if echo "$RESPONSE" | grep -q "200 OK"
-then
-    curl -s "$1"
-fi
+# if echo "$RESPONSE" | grep -q "200 OK"
+# then
+#     curl -s "$1"
+# fi
+curl -s -X GET "$1"
