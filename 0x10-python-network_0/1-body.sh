1fdaa144c718fb1e965d503d9a1fbea50e0c56e2
Tau <taumasaire@gmail.com>
2024-02-29 15:06:01 +0200

    delete request script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request and displays the body
-
-# RESPONSE=$(curl -sI "$1")
-
-# if echo "$RESPONSE" | grep -q "200 OK"
-# then
-#     curl -s "$1"
-# fi
-curl -s -X GET "$1"
+RESPONSE=$(curl -sI "$1"); if echo "$RESPONSE" | grep -q "200 OK"; then curl -s "$1"; fi
