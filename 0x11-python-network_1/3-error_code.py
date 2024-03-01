691cd0fc28224dffb375195a0db66e44efd15ec6
Tau <taumasaire@gmail.com>
2024-03-01 16:17:33 +0200

    post an email

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     req = request.Request(sys.argv[1])
     try:
         with request.urlopen(req) as response:
-            print(response.read().decode('utf-8'))
+            body = response.read().decode('utf-8')
     except error.HTTPError as e:
         print('Error code: ', e.code)
     except error.URLError as e:
         print('Error code: ', e.reason)
+    else:
+        print(body)
