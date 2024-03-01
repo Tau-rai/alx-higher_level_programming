acb8f0586e589b2d8646cdcbccc59a02a3c26fcc
Tau <taumasaire@gmail.com>
2024-03-01 15:39:12 +0200

    error handling script

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains a script that takes in a url, send a request and
+displays the body of the response
+"""
+
+
+import sys
+from urllib import request, error
+
+
+if __name__ == "__main__":
+    req = request.Request(sys.argv[1])
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: ', e.code)
