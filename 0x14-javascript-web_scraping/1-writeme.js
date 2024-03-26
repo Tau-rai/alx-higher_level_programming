16a2d9d1f7088a0dbc55f939899aaf12e94ec198
Tau <taumasaire@gmail.com>
2024-03-26 18:38:16 +0200

    write me script

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const fs = require('fs');
+
+const file = argv[2];
+const dataString = argv[3];
+fs.writeFile(file, dataString, 'utf8', err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('File successfully written');
+  }
+});
